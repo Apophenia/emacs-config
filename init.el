88a8ecbdc33ad4ef255e3d235c92e36ca59e6925
Lyndsey Moulds <lmoulds@mm-mac-5039.local>
2014-10-07 19:17:18 -0400

    Remove marmalade

--- init.el
+++ init.el
@@ -4,7 +4,6 @@
 
 (package-initialize)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 (add-to-list 'load-path "~/.emacs.d/manual/")
 (add-to-list 'load-path "~/.emacs.d/etc/")
